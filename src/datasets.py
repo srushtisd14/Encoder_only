355e4582d09a5d8440da60d7a2e6825c16d36a19
srushtisd14 <srushtisd14@gmail.com>
2026-08-11 15:58:53 +0530

    Fix indentation and update pipeline

--- src/datasets.py
+++ src/datasets.py
@@ -1,43 +1,7 @@
-DATASET_CONFIG = {
-    "sst2": {
-        "name": "glue",
-        "subset": "sst2",
-        "text1": "sentence",
-        "text2": None
-    },
-
-    "mrpc": {
-        "name": "glue",
-        "subset": "mrpc",
-        "text1": "sentence1",
-        "text2": "sentence2"
-    },
-
-    "qnli": {
-        "name": "glue",
-        "subset": "qnli",
-        "text1": "question",
-        "text2": "sentence"
-    },
-
-    "mnli": {
-        "name": "glue",
-        "subset": "mnli",
-        "text1": "premise",
-        "text2": "hypothesis"
-    },
-
-    "imdb": {
-        "name": "stanfordnlp/imdb",
-        "subset": None,
-        "text1": "text",
-        "text2": None
-    },
-
-    "ag_news": {
-        "name": "fancyzhx/ag_news",
-        "subset": None,
-        "text1": "text",
-        "text2": None
-    }
-}
+MODEL_CONFIG = {"bert-base": "bert-base-uncased", 
+                "roberta-base": "roberta-base", 
+                "distilbert": "distilbert-base-uncased", 
+                "albert-base-v2": "albert-base-v2", 
+                "electra-base": "google/electra-base-discriminator", 
+                "deberta-v3-base": "microsoft/deberta-v3-base",
+                "mpnet-base": "microsoft/mpnet-base"}
