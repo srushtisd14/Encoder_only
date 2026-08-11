9236d60232e2297d3b077cdc7243661de06fdb8e
srushtisd14 <srushtisd14@gmail.com>
2026-08-11 15:49:24 +0530

    Fix indentation and update pipeline

--- src/models.py
+++ src/models.py
@@ -1,29 +1,9 @@
 MODEL_CONFIG = {
-    "bert-base": {
-        "name": "bert-base-uncased"
-    },
-
-    "roberta-base": {
-        "name": "roberta-base"
-    },
-
-    "distilbert": {
-        "name": "distilbert-base-uncased"
-    },
-
-    "albert-base-v2": {
-        "name": "albert-base-v2"
-    },
-
-    "electra-base": {
-        "name": "google/electra-base-discriminator"
-    },
-
-    "deberta-v3-base": {
-        "name": "microsoft/deberta-v3-base"
-    },
-
-    "mpnet-base": {
-        "name": "microsoft/mpnet-base"
-    }
+    "bert-base": "bert-base-uncased",
+    "roberta-base": "roberta-base",
+    "distilbert": "distilbert-base-uncased",
+    "albert-base-v2": "albert-base-v2",
+    "electra-base": "google/electra-base-discriminator",
+    "deberta-v3-base": "microsoft/deberta-v3-base",
+    "mpnet-base": "microsoft/mpnet-base",
 }
